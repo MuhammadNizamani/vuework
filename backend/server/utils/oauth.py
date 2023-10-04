486f9035c676cd87ca31f93b79297677c3173321
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-10-04 22:49:19 +0500

    adding Oauth

--- backend/server/utils/oauth.py
+++ backend/server/utils/oauth.py
@@ -1,6 +1,11 @@
-from jose import JWSError, jwt
+from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from server.schemas import token_schemas
+from fastapi import Depends, status, HTTPException
+from server.models.models import Users, session
+from fastapi.security import OAuth2PasswordBearer
 import os
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 # Seceret key
 #Algorithm
 # expiration time
@@ -15,3 +20,27 @@ def create_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def verify_access_token(token: str, credentials_exception):
+
+    try:
+
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = token_schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+
+    return token_data
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+
+    token = verify_access_token(token, credentials_exception)
+
+    user = session.query(Users).filter(Users.user_id == token.id).first()
+
+    return user
